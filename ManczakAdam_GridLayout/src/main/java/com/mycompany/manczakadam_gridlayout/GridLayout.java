e973c6c56cca09cd4b783ce26246564ce906fa47
Adam2PTN <77607603+Adam2PTN@users.noreply.github.com>
2022-01-31 09:34:24 +0100

    kolejny commit

--- ManczakAdam_GridLayout/src/main/java/com/mycompany/manczakadam_gridlayout/GridLayout.java
+++ ManczakAdam_GridLayout/src/main/java/com/mycompany/manczakadam_gridlayout/GridLayout.java
@@ -26,17 +26,156 @@ public class GridLayout extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jTextField1 = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton21 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton13 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton10 = new javax.swing.JButton();
+        jButton11 = new javax.swing.JButton();
+        jButton12 = new javax.swing.JButton();
+        jButton14 = new javax.swing.JButton();
+        jButton15 = new javax.swing.JButton();
+        jButton23 = new javax.swing.JButton();
+        jButton22 = new javax.swing.JButton();
+        jButton17 = new javax.swing.JButton();
+        jButton16 = new javax.swing.JButton();
+        jButton18 = new javax.swing.JButton();
+        jButton19 = new javax.swing.JButton();
+        jButton20 = new javax.swing.JButton();
+        jButton24 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new java.awt.GridLayout());
+
+        jTextField1.setBackground(new java.awt.Color(0, 0, 0));
+        jTextField1.setFont(new java.awt.Font("Segoe UI", 0, 30)); // NOI18N
+        jTextField1.setForeground(new java.awt.Color(255, 255, 255));
+        jTextField1.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        jTextField1.setText("0");
+        jPanel1.add(jTextField1);
+
+        jPanel2.setLayout(new java.awt.GridLayout(6, 4, 4, 4));
+
+        jButton1.setBackground(new java.awt.Color(0, 0, 0));
+        jButton1.setText("mc");
+        jPanel2.add(jButton1);
+
+        jButton2.setBackground(new java.awt.Color(0, 0, 0));
+        jButton2.setText("m+");
+        jPanel2.add(jButton2);
+
+        jButton9.setBackground(new java.awt.Color(0, 0, 0));
+        jButton9.setText("m-");
+        jPanel2.add(jButton9);
+
+        jButton5.setBackground(new java.awt.Color(0, 0, 0));
+        jButton5.setText("mr");
+        jPanel2.add(jButton5);
+
+        jButton3.setBackground(new java.awt.Color(0, 0, 0));
+        jButton3.setText("C");
+        jPanel2.add(jButton3);
+
+        jButton21.setBackground(new java.awt.Color(0, 0, 0));
+        jButton21.setText("/");
+        jPanel2.add(jButton21);
+
+        jButton4.setBackground(new java.awt.Color(0, 0, 0));
+        jButton4.setText("X");
+        jPanel2.add(jButton4);
+
+        jButton13.setBackground(new java.awt.Color(0, 0, 0));
+        jButton13.setText("<-");
+        jPanel2.add(jButton13);
+
+        jButton6.setBackground(new java.awt.Color(0, 0, 0));
+        jButton6.setText("7");
+        jPanel2.add(jButton6);
+
+        jButton7.setBackground(new java.awt.Color(0, 0, 0));
+        jButton7.setText("8");
+        jPanel2.add(jButton7);
+
+        jButton8.setBackground(new java.awt.Color(0, 0, 0));
+        jButton8.setText("9");
+        jPanel2.add(jButton8);
+
+        jButton10.setBackground(new java.awt.Color(0, 0, 0));
+        jButton10.setText("√");
+        jPanel2.add(jButton10);
+
+        jButton11.setBackground(new java.awt.Color(0, 0, 0));
+        jButton11.setText("4");
+        jPanel2.add(jButton11);
+
+        jButton12.setBackground(new java.awt.Color(0, 0, 0));
+        jButton12.setText("5");
+        jPanel2.add(jButton12);
+
+        jButton14.setBackground(new java.awt.Color(0, 0, 0));
+        jButton14.setText("6");
+        jPanel2.add(jButton14);
+
+        jButton15.setBackground(new java.awt.Color(0, 0, 0));
+        jButton15.setText("-");
+        jPanel2.add(jButton15);
+
+        jButton23.setBackground(new java.awt.Color(0, 0, 0));
+        jButton23.setText("1");
+        jPanel2.add(jButton23);
+
+        jButton22.setBackground(new java.awt.Color(0, 0, 0));
+        jButton22.setText("2");
+        jPanel2.add(jButton22);
+
+        jButton17.setBackground(new java.awt.Color(0, 0, 0));
+        jButton17.setText("3");
+        jPanel2.add(jButton17);
+
+        jButton16.setBackground(new java.awt.Color(0, 0, 0));
+        jButton16.setText("+");
+        jPanel2.add(jButton16);
+
+        jButton18.setBackground(new java.awt.Color(0, 0, 0));
+        jButton18.setText("%");
+        jPanel2.add(jButton18);
+
+        jButton19.setBackground(new java.awt.Color(0, 0, 0));
+        jButton19.setText("0");
+        jPanel2.add(jButton19);
+
+        jButton20.setBackground(new java.awt.Color(0, 0, 0));
+        jButton20.setText(".");
+        jPanel2.add(jButton20);
+
+        jButton24.setBackground(new java.awt.Color(0, 0, 0));
+        jButton24.setText("=");
+        jPanel2.add(jButton24);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 278, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
@@ -78,5 +217,32 @@ public class GridLayout extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton10;
+    private javax.swing.JButton jButton11;
+    private javax.swing.JButton jButton12;
+    private javax.swing.JButton jButton13;
+    private javax.swing.JButton jButton14;
+    private javax.swing.JButton jButton15;
+    private javax.swing.JButton jButton16;
+    private javax.swing.JButton jButton17;
+    private javax.swing.JButton jButton18;
+    private javax.swing.JButton jButton19;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton20;
+    private javax.swing.JButton jButton21;
+    private javax.swing.JButton jButton22;
+    private javax.swing.JButton jButton23;
+    private javax.swing.JButton jButton24;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
